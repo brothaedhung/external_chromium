9a0e9c058c9b1b93c6099073c9c4e37ea46d0044
edgar.benson <edbenson@hotmail.com>
2013-11-11 15:12:58 -0800

    chromium: Fix aliasing violations
    Change-Id: I6117b0a1e56e2a427ff2c2226b6274749f618425
    Signed-off-by: Bernhard Rosenkraenzer <Bernhard.Rosenkranzer@linaro.org>
    chromium: Fix aliasing violations
    
    Signed-off-by: Bernhard Rosenkraenzer <Bernhard.Rosenkranzer@linaro.org>

--- Android.mk
+++ Android.mk
@@ -482,7 +482,7 @@ $(GEN):
 LOCAL_GENERATED_SOURCES += $(GEN)
 
 LOCAL_CFLAGS := -DHAVE_CONFIG_H -DANDROID -DEXPAT_RELATIVE_PATH -DALLOW_QUOTED_COOKIE_VALUES -DCOMPONENT_BUILD -DGURL_DLL
-LOCAL_CPPFLAGS := -Wno-sign-promo -Wno-missing-field-initializers -fvisibility-inlines-hidden
+LOCAL_CPPFLAGS := -Wno-sign-promo -Wno-missing-field-initializers -fvisibility-inlines-hidden -fno-strict-aliasing
 
 # Just a few definitions not provided by bionic.
 LOCAL_CFLAGS += -include "android/prefix.h"
